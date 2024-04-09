2fd8c508c2badf27ab8e35c7b029377a3b184a8a
Raulx103 <raulx103@gmail.com>
2024-04-09 13:08:18 +0530

    cloudinary config file added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
+import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import blogRouter from "./routes/blogRoutes.js";
 import path from "path";
@@ -11,6 +11,7 @@ import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
+connectCloudinary();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
@@ -21,13 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", userRouter);
 app.use("/api/blogs", blogRouter);
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
-});
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
