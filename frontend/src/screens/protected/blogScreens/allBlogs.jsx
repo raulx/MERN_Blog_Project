c06dda788114b1d4a0f4ead0ad0063ab45c1c8ee
Raulx103 <raulx103@gmail.com>
2025-02-08 18:47:16 +0530

    queringFix done

--- frontend/src/screens/protected/blogScreens/allBlogs.jsx
+++ frontend/src/screens/protected/blogScreens/allBlogs.jsx
@@ -1,168 +1,36 @@
-// import { useSelector, useDispatch } from "react-redux";
-// import { useGetBlogsQuery } from "../../../store";
-// import { useState, useEffect, useRef } from "react";
-// import { addBlog, setPrevData } from "../../../store";
-// import Card from "../../../components/card";
-// import CardSkeleton from "../../../components/cardSkeleton";
-// import { FaFilter, FaChevronDown, FaChevronLeft } from "react-icons/fa";
 import { blogCategories } from "../../../utils/variables";
-
 import { useLazyGetBlogsQuery } from "../../../store";
 import Card from "../../../components/card";
 import CardSkeleton from "../../../components/cardSkeleton";
 import { useEffect, useState, useRef } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { FaFilter, FaChevronDown, FaChevronLeft } from "react-icons/fa";
-
-// function BlogsIndex() {
-//   const { blogs, prevData, currentPage, pageSize } = useSelector((state) => {
-//     return state.blogs;
-//   });
-
-//   const { data, isFetching } = useGetBlogsQuery({
-//     page: currentPage,
-//     pageSize: pageSize,
-//   });
-//   const dispatch = useDispatch();
-//   const dropDowm = useRef();
-
-//   const [filterNav, setFilterNav] = useState({
-//     isOpen: false,
-//     currentCategory: "all",
-//   });
-
-//   useEffect(() => {
-//     const handler = (event) => {
-//       if (filterNav.isOpen && !dropDowm.current.contains(event.target)) {
-//         setFilterNav((prevValue) => {
-//           return { ...prevValue, isOpen: false };
-//         });
-//       }
-//     };
-//     document.addEventListener("click", handler, true);
-
-//     return () => {
-//       document.removeEventListener("click", handler, true);
-//     };
-//   }, [filterNav.isOpen]);
-
-//   useEffect(() => {
-//     if (data) {
-//       if (JSON.stringify(prevData) != JSON.stringify(data.data)) {
-//         dispatch(addBlog(data.data));
-//         dispatch(setPrevData(data.data));
-//       }
-//     }
-//   }, [data, dispatch, prevData]);
-
-//   let blogData = blogs;
-
-//   if (filterNav.currentCategory != "all") {
-//     blogData = blogData.filter((blog) => {
-//       return blog.category === filterNav.currentCategory;
-//     });
-//   }
-
-//   const handleClick = (e) => {
-//     const categoryChoosen = e.target.innerText.toLowerCase();
-//     setFilterNav((prevValue) => {
-//       return {
-//         ...prevValue,
-//         currentCategory: categoryChoosen,
-//         isOpen: !prevValue,
-//       };
-//     });
-//   };
-
-//   return (
-//     <>
-//       {filterNav.isOpen ? (
-//         <div
-//           ref={dropDowm}
-//           className="border shadow-xl p-2 bg-slate-100 rounded-lg flex flex-col gap-2 absolute z-10 md:right-20 right-4 w-80 md:top-16 top-28 md:w-96"
-//         >
-//           {blogCategories.map((d) => {
-//             return (
-//               <p
-//                 className={`text-xl  w-full h-full cursor-pointer py-2 capitalize px-4 rounded-xl  ${
-//                   d.category === filterNav.currentCategory
-//                     ? "bg-slate-600 text-white"
-//                     : "hover:bg-slate-500 hover:text-white"
-//                 }`}
-//                 key={d.id}
-//                 onClick={(e) => handleClick(e)}
-//               >
-//                 {d.category}
-//               </p>
-//             );
-//           })}
-//         </div>
-//       ) : null}
-
-//       <div className="flex justify-between items-center mt-6 md:mt-4">
-//         <h1 className="text-2xl text-gray-400 font-extrabold uppercase">
-//           Results
-//         </h1>
-//         <div
-//           className="flex md:gap-6 gap-0 items-center cursor-pointer mr-10 "
-//           onClick={() => {
-//             setFilterNav((prevValue) => {
-//               return { ...prevValue, isOpen: !prevValue.isOpen };
-//             });
-//           }}
-//         >
-//           <div className="flex gap-4 items-center p-2">
-//             <p className="text-2xl hidden md:block text-gray-400 font-extrabold uppercase">
-//               Sort By
-//             </p>
-//             <FaFilter className="text-2xl" />
-//           </div>
-
-//           <div
-//             ref={dropDowm}
-//             className="h-full border-2 md:w-72 w-60 rounded py-2 px-4 md:text-lg text-base uppercase flex items-center justify-between"
-//           >
-//             <p>{filterNav.currentCategory}</p>
-//             <div>
-//               {filterNav.isOpen ? <FaChevronDown /> : <FaChevronLeft />}
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-
-//       <div className="flex flex-wrap w-full justify-evenly gap-4  gap-y-16 mt-4">
-//         {blogData.map((blog, index) => {
-//           return <Card key={index} cardData={blog} />;
-//         })}
-//         {filterNav.currentCategory === "all" ? (
-//           <>{isFetching ? <CardSkeleton times={pageSize} /> : null}</>
-//         ) : null}
-//       </div>
-//     </>
-//   );
-// }
+import { useSearchParams } from "react-router-dom";
 
 function AllBlogs() {
   const [nextPage, setNextPage] = useState(2);
+
   const [hasMore, setHasMore] = useState(true);
+
   const dropDowm = useRef();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const category = searchParams.get("category") || "all";
 
   const [blogs, setBlogs] = useState([]);
 
   const [getBlogs, { isLoading }] = useLazyGetBlogsQuery();
-  const [filterNav, setFilterNav] = useState({
-    isOpen: false,
-    currentCategory: "all",
-  });
+  const [isOpen, setIsOpen] = useState(false);
 
-  const pageLength = 10;
+  const pageLength = 12;
 
   const fetchMoreBlogs = async () => {
     try {
       const res = await getBlogs({
         page: nextPage,
         pageSize: pageLength,
-        category: filterNav.currentCategory,
+        category: category,
       });
       if (res.data) {
         setBlogs((prevValue) => [...prevValue, ...res.data.data]);
@@ -182,7 +50,7 @@ function AllBlogs() {
         const res = await getBlogs({
           page: 1,
           pageSize: pageLength,
-          category: filterNav.currentCategory,
+          category: category,
         });
         if (res.data) {
           setBlogs(() => [...res.data.data]);
@@ -191,16 +59,15 @@ function AllBlogs() {
         console.log(err);
       }
     };
+
     fetchInitialBlogs();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [category]);
 
   useEffect(() => {
     const handler = (event) => {
-      if (filterNav.isOpen && !dropDowm.current.contains(event.target)) {
-        setFilterNav((prevValue) => {
-          return { ...prevValue, isOpen: false };
-        });
+      if (isOpen && !dropDowm.current.contains(event.target)) {
+        setIsOpen(false);
       }
     };
 
@@ -209,40 +76,26 @@ function AllBlogs() {
     return () => {
       document.removeEventListener("click", handler, true);
     };
-  }, [filterNav.isOpen]);
+  }, [isOpen]);
 
-  const handleClick = async (e) => {
-    const categoryChoosen = e.target.innerText.toLowerCase();
-
-    setFilterNav((prevValue) => {
-      return {
-        ...prevValue,
-        currentCategory: categoryChoosen,
-        isOpen: !prevValue,
-      };
-    });
-
-    try {
-      const res = await getBlogs({
-        page: 1,
-        category: categoryChoosen,
-        pageSize: pageLength,
-      });
-      console.log(res);
-
-      if (res.data) {
-        setBlogs(res.data.data);
-        setHasMore(true);
-        setNextPage(2);
-      }
-    } catch (error) {
-      console.log(error);
+  const handleCategoryChange = async (category) => {
+    const params = new URLSearchParams(searchParams);
+    if (category === "all") {
+      params.delete("category");
+    } else {
+      params.set("category", category);
     }
+    setSearchParams(params);
+
+    setIsOpen(!isOpen);
+
+    setHasMore(true);
+    setNextPage(2); // reset nextpage from start
   };
 
   return (
     <div className="flex flex-col sm:w-11/12 mx-auto gap-4 py-6 h-screen -z-10">
-      {filterNav.isOpen ? (
+      {isOpen ? (
         <div
           ref={dropDowm}
           className="border shadow-xl p-2 bg-slate-100 rounded-lg flex flex-col gap-2 absolute z-10 md:right-20 right-4 w-80 md:w-96"
@@ -251,12 +104,12 @@ function AllBlogs() {
             return (
               <p
                 className={`text-xl  w-full h-full cursor-pointer py-2 capitalize px-4 rounded-xl  ${
-                  d.category === filterNav.currentCategory
+                  d.category === category
                     ? "bg-slate-600 text-white"
                     : "hover:bg-slate-500 hover:text-white"
                 }`}
                 key={d.id}
-                onClick={(e) => handleClick(e)}
+                onClick={() => handleCategoryChange(d.category)}
               >
                 {d.category}
               </p>
@@ -269,10 +122,11 @@ function AllBlogs() {
         <h1 className="text-2xl text-gray-400 font-extrabold uppercase">
           Results
         </h1>
+
         <div
           className="flex md:gap-6 gap-0 items-center cursor-pointer mr-10 "
           onClick={() => {
-            setFilterNav((prevValue) => {
+            setIsOpen((prevValue) => {
               return { ...prevValue, isOpen: !prevValue.isOpen };
             });
           }}
@@ -288,13 +142,12 @@ function AllBlogs() {
             ref={dropDowm}
             className="h-full border-2 md:w-72 w-60 rounded py-2 px-4 md:text-lg text-base uppercase flex items-center justify-between"
           >
-            <p>{filterNav.currentCategory}</p>
-            <div>
-              {filterNav.isOpen ? <FaChevronDown /> : <FaChevronLeft />}
-            </div>
+            <p>{category}</p>
+            <div>{isOpen ? <FaChevronDown /> : <FaChevronLeft />}</div>
           </div>
         </div>
       </div>
+
       {isLoading ? (
         <div className="flex flex-wrap gap-8 justify-between p-4">
           <CardSkeleton times={pageLength} />
